f6d292b4f2109b02b3f485944def4f5d63811f49
KurisuLim <kazuki1002003@yahoo.com>
2018-11-05 12:14:24 -0800

    adds the p method

---
+++ the_p_method.rb
@@ -0,0 +1,15 @@
+puts "Steven Seagal"
+
+# Display as string
+p "Steaven Seagal"
+
+# Use of the p method
+
+puts "Hi there, this is a 
+big line break"
+
+p "Hi there, this is a 
+big line break"
+
+puts 5
+p 5
