6782818edc40a11828509cd8290f7cb7cf88ef4c
KurisuLim <kazuki1002003@yahoo.com>
2018-11-05 14:41:12 -0800

    adds object method

---
+++ intro_to_object_methods.rb
@@ -0,0 +1,21 @@
+# String
+p "Hello World".length # display 11 characters 
+p "hello world".upcase # Capitalize all letters
+p "HELLO WORLD".downcase # Lower case all characters
+
+foo = "Hello Hello"
+
+foo.length
+foo.upcase
+foo.downcase
+
+puts
+puts
+
+# Number
+p 10.next # increase by 1, now the number is 11
+p -1.next # gets 0
+
+puts "Hello world"
+puts "Hello world".inspect # Shows string and same to p "Hello World"
+                          # Displays "Hello World"
