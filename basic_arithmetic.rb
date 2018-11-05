6c0407d7006cb0500100ef4ddbfc826e285aeadf
KurisuLim <kazuki1002003@yahoo.com>
2018-11-05 12:25:57 -0800

    basic arithmetic

---
+++ basic_arithmetic.rb
@@ -0,0 +1,35 @@
+# Expression 
+
+# Additon
+p 1 + 4
+
+#Subtraction
+p 10 - 6
+
+# Multiplication
+p 3 * 4
+
+#Addition operator can be use for concat
+p "School " + "bus"
+
+# Division
+p 10 / 5
+p 12 / 5 # 2.4 but chops off the decimal point
+
+p 12.0 / 5.0 # add .0 for float
+p 12 / 5.0
+p 12.0 / 5
+
+# exponents
+p 2 * 2 * 2 * 2
+p 2 ** 5 # 2 the power of 5 is 32
+p 3 ** 3 # 2 by the poer of 3 is 27
+
+# Modular or remainder
+p 5 % 2 # 1
+p 6 % 2 # 0
+p 14 % 4 # 2
+
+# should have put 0 if less than 1
+p 0.5 / 2
+p -0.75 / 2
