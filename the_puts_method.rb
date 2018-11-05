ed4ce60d000723858037c7c72ccb57ec46ec6445
KurisuLim <kazuki1002003@yahoo.com>
2018-11-05 12:04:41 -0800

    adds the puts methods

---
+++ the_puts_method.rb
@@ -0,0 +1,28 @@
+# String
+puts "Hello word"
+puts "I'm alive and well!"
+
+puts "I bought 5 dollars at the store."
+puts "I made $65 dollars or a profit of 25%!"
+
+# Number
+puts 5
+
+# Floats
+puts 3.14159
+
+# Addition Computation
+puts 4 + 3
+
+# String and Number
+# puts "4" + 3
+
+# String tand String(append or concat)
+puts "4" + "3"
+
+# Add white spaces
+puts
+puts
+puts
+
+puts "See, there's a bunch of line breaks above!"
