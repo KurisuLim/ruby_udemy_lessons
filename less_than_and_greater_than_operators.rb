221686971a7bd825a29286642d68591120cc7400
KurisuLim <kazuki1002003@yahoo.com>
2018-11-07 07:46:35 -0800

    less_than_and_greater_than_operators

---
+++ less_than_and_greater_than_operators.rb
@@ -0,0 +1,20 @@
+p 1 < 8 # returns true
+p 10 < 15 # returns true
+
+p 12 < 5 # returns false
+
+p 1 <= 5 # returns true
+p 13 <= 5 # returns false
+
+
+p 10 > 5 # returns true
+protected_methods -1 > -5 # returns true
+p 10 > 20 # returns false
+
+
+p 12.83 < 21.14 # returns true
+p 12.83 > 21.14 # returns false
+
+p 8 >= 5 # returns true
+p 8 >= 10 # returns false
+p 8 >= 8 # returns true
