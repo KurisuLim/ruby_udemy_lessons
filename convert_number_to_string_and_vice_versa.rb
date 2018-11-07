dfd1d2c1a1281212e63568d72fba3464e8408035
KurisuLim <kazuki1002003@yahoo.com>
2018-11-06 22:31:21 -0800

    adds convert num to string and vice versa

---
+++ convert_number_to_string_and_vice_versa.rb
@@ -0,0 +1,30 @@
+str = "5"
+p str
+p str.class
+
+p str.to_i #convert to integer
+p str.to_i.class
+
+p str.to_f #convert to float
+p str.to_f.class
+
+number = 10
+p number
+p number.class
+p number.to_s # converts to string
+p number.to_s.class
+
+pi = 3.14
+
+p pi
+p pi.class
+p pi.to_i #converts to integer
+          #result would be 3
+p pi.to_i.class
+
+p pi.to_s
+p pi.to_s.class
+
+p "5".to_s.class
+p 10.to_i.class
+p 99.99.to_f.class
