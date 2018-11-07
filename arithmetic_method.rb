a60502064275c3500f52752c2cc48a781b4e5c39
KurisuLim <kazuki1002003@yahoo.com>
2018-11-07 07:56:16 -0800

    add arithmetic method

---
+++ arithmetic_method.rb
@@ -0,0 +1,8 @@
+p 1.+(2) #returns 3
+p 10.-(5) # returns 5
+p 3.*(4) # returns 12
+p 10./(5) # returns 2
+p 12.0./(5.0) # returns 2.4
+p 10.div(5) # returns 2
+p 10.%(3) # returns 1
+p 10.modulo(3) #returns 1
