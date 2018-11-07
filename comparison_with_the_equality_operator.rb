0368af47c3ba69fc4dbf88683f5048a9b3aa3953
KurisuLim <kazuki1002003@yahoo.com>
2018-11-07 07:29:15 -0800

    comparison_with_the_equality_operator

---
+++ comparison_with_the_equality_operator.rb
@@ -0,0 +1,19 @@
+p 10 == 10 # returns true
+p 10 == '10' # return false
+p '10' == '10' # return true
+p 'Hello world' == 'Hi there' #return false
+p 5.class # integer
+p 5.0.class # float
+p 5.to_f.class # 5.0 and float
+p 5 == 5.0 # return true
+
+
+a = 10
+b = 5
+c = 10
+
+p a == c
+p a == b
+p c == a
+p b == c
+
