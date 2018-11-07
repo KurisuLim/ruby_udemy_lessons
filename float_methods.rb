d8a4e83afc9b05c72f03eab42b1a3ea310d765dc
KurisuLim <kazuki1002003@yahoo.com>
2018-11-07 12:26:23 -0800

    adds float methods

---
+++ float_methods.rb
@@ -0,0 +1,12 @@
+p 10.9.to_i.class #float
+
+p 10.5.floor #10
+p 10.5.ceil #11
+
+p 3.14159.round # returns 3
+p 3.6.round # returns 4
+
+p 3.14159.round(2) #returns 3.14
+
+p 35.67.abs # distance number from 0
+p -35.65.abs
