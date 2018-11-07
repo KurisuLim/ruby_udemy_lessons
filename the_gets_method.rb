691703ff03fefb7cf469dcbd2fa7cd8951be0fff
KurisuLim <kazuki1002003@yahoo.com>
2018-11-06 22:01:20 -0800

    adds gets method

---
+++ the_gets_method.rb
@@ -0,0 +1,8 @@
+puts "Hi, what's your name?"
+name = gets.chomp
+
+puts "Great! How old are you?"
+age = gets.chomp.to_i #to_i to convert to interger
+                      #to_s to convert to string
+
+puts "Cool, so your name is #{name} and you're age is #{age}."
