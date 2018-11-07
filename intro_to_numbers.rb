08a92d1822916cdf7b53100ae3e925a6967f134e
KurisuLim <kazuki1002003@yahoo.com>
2018-11-06 22:21:48 -0800

    adds intro to numbers

---
+++ intro_to_numbers.rb
@@ -0,0 +1,13 @@
+# integer
+p 5.next
+p 0.next
+p 100.class
+p -837
+
+# float
+p 5.987.class
+p 2.71 # cannot have next method
+p -10.23
+
+# big number
+p 9999999999999999999999999999999999999999999.class
