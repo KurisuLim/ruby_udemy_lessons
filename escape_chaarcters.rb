e4f0a25b87436f20e90548f076579b04ffde0f07
KurisuLim <kazuki1002003@yahoo.com>
2019-01-28 23:20:25 -0800

    added escape characters spec

---
+++ escape_chaarcters.rb
@@ -0,0 +1,18 @@
+# escape characters
+puts 'hello world'
+puts 'hello world'
+
+puts "juliet said 'goodbye' to romeo"
+puts 'juliet said "goodbye" to romeo'
+# \' or \" for quotes
+puts "juliet said \"goodbye\" to romeo"
+puts 'juliet said \'goodbye\' to romeo'
+
+# \n next line
+puts "let\'s add a line break\nright here"
+
+result = "let\'s add a line break\nright here"
+puts result
+p result
+# \t tabs
+puts "\tinsert a tab right here"
